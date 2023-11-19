2b275129151890dd317eecccbf53dc3ad442b22d
hardtenderblade <matsenkodenis0@gmail.com>
2023-11-19 15:54:57 +0100

    Moving record class

--- Diary.cs
+++ Diary.cs
@@ -1,4 +1,17 @@
 
+public class Record {
+    public DateTime Date { get; set; }
+    public string Text { get; set; }
+    public Record(DateTime date, string text) {
+        Date = date;
+        Text = text;
+    }
+    public void Display() {
+        Console.WriteLine($"Date: {Date.ToShortDateString()}\n");
+        Console.WriteLine($"Text: {Text}\n\n");
+    }
+}
+
 public class Node
 {
     public Record Data { get; set; }
